568148c6f34079782d7b0f75ea7fe452bdac2a55
julianwaimann <jwaimann@makingsense.com>
2016-07-28 16:32:06 -0300

    Add Split domain related classes

---
+++ NetSDK/Domain/BetweenData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NetSDK.Domain
+{
+    public class BetweenData
+    {
+        public DataTypeEnum? DataType { get; set; }
+        public long Start { get; set; }
+        public long End { get; set; }
+    }
+}
---
+++ NetSDK/Domain/ConditionDefinition.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NetSDK.Domain
+{
+    public class ConditionDefinition
+    {
+        public MatcherGroupDefinition MatcherGroup { get; set; }
+        public List<PartitionDefinition> Partitions { get; set; }
+        public string Label { get; set; }
+    }
+}
