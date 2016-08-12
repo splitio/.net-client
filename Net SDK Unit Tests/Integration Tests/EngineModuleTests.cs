1858c5bfcd7f500561dfea3bcac55c8de3a25ccd
julianwaimann <jwaimann@makingsense.com>
2016-08-12 11:24:04 -0300

    Implement integration test cases for whitelist condition and matcher

--- Net SDK Unit Tests/Integration Tests/EngineModuleTests.cs
+++ Net SDK Unit Tests/Integration Tests/EngineModuleTests.cs
@@ -48,6 +48,7 @@ namespace Net_SDK_Unit_Tests.Integration_Tests
             ParsedSplit split = selfRefreshingSplitFetcher.Fetch("test_jw");
             ParsedSplit split2 = selfRefreshingSplitFetcher.Fetch("test_jw2");
             ParsedSplit split3 = selfRefreshingSplitFetcher.Fetch("test_jw3");
+            ParsedSplit split4 = selfRefreshingSplitFetcher.Fetch("test_jw4");
 
             Splitter splitter = new Splitter();
             Engine engine = new Engine(splitter);
@@ -55,7 +56,7 @@ namespace Net_SDK_Unit_Tests.Integration_Tests
             //Act
             //get treatment for split test_jw
             Dictionary<string, object> dict = new Dictionary<string,object>();
-            dict.Add("date", 1470944122);
+            dict.Add("date", 1470960000000);
             var result1 = engine.GetTreatment("1f84e5ddb06a3e66145ccfc1aac247", split, dict);
 
             Dictionary<string, object> dict2 = new Dictionary<string, object>();
@@ -71,8 +72,25 @@ namespace Net_SDK_Unit_Tests.Integration_Tests
             dict4.Add("date", 9);
             var result4 = engine.GetTreatment("xadcc", split2, dict4);
 
-            //get treatment for split tes_jw3
+            //get treatment for split test_jw3
             var result5 = engine.GetTreatment("xadcc", split3, dict4);
+
+
+            //get treatment for split test_jw4
+            Dictionary<string, object> dict5 = new Dictionary<string, object>();
+            dict5.Add("date", 9);
+            dict5.Add("test", "acdefx");
+            var result6 = engine.GetTreatment("xadcc", split4, dict5);
+
+            Dictionary<string, object> dict6 = new Dictionary<string, object>();
+            dict6.Add("date", 9);
+            dict6.Add("test", "azzdefx");
+            var result7 = engine.GetTreatment("xadcscdcc", split4, dict6);
+
+            Dictionary<string, object> dict7 = new Dictionary<string, object>();
+            dict7.Add("date", 9);
+            dict7.Add("test", "azzdefx");
+            var result8 = engine.GetTreatment("abcdef", split4, dict7);
 
             //Assert
             Assert.IsTrue(result1 == "on"); //date is equal
@@ -80,6 +98,9 @@ namespace Net_SDK_Unit_Tests.Integration_Tests
             Assert.IsTrue(result3 == "on"); //is in segment payed
             Assert.IsTrue(result4 == "off"); // default
             Assert.IsTrue(result5 == "off"); //<killed> default
+            Assert.IsTrue(result6 == "on"); // in whitelist
+            Assert.IsTrue(result7 == "off"); // default
+            Assert.IsTrue(result8 == "on"); // included user abcdef (whitelist)
         }
     }
 }
