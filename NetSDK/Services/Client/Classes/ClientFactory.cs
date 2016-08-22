cece54eed3cb4336f76ecf08d5d643997b0704b2
julianwaimann <jwaimann@makingsense.com>
2016-08-22 17:59:51 -0300

    Implement ClientFactory

---
+++ NetSDK/Services/Client/Classes/ClientFactory.cs
@@ -0,0 +1,21 @@
+﻿using NetSDK.Services.Client.Classes;
+using NetSDK.Services.Client.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Splitio.Services.Client.Classes
+{
+    public class ClientFactory
+    {
+        public IClient BuildApiClient(string apiKey, ConfigurationOptions options)
+        {
+            if (apiKey == "localhost")
+            {
+                return new LocalhostClient(options.LocalhostFilePath);
+            }
+            return new SelfRefreshingClient(apiKey);
+        }
+    }
+}
