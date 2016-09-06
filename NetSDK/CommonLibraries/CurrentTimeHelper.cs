905d49650895df51b757679f11fbc9790af11d27
julianwaimann <jwaimann@makingsense.com>
2016-09-06 16:51:55 -0300

    Add metrics logging for split and segment change fetchers

---
+++ NetSDK/CommonLibraries/CurrentTimeHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Splitio.CommonLibraries
+{
+    public static class CurrentTimeHelper
+    {
+        public static long CurrentTimeMillis()
+        {
+            var Jan1st1970 = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            return (long)(DateTime.UtcNow - Jan1st1970).TotalMilliseconds;
+        }
+    }
+}
--- NetSDK/Services/SplitFetcher/Classes/SplitSdkApiClient.cs
+++ NetSDK/Services/SplitFetcher/Classes/SplitSdkApiClient.cs
@@ -8,6 +8,8 @@ using System.Threading.Tasks;
 using System.Net;
 using Splitio.Services.SplitFetcher.Interfaces;
 using log4net;
+using Splitio.Services.Metrics.Interfaces;
+using System.Diagnostics;
 
 namespace Splitio.Services.SplitFetcher
 {
@@ -16,28 +18,52 @@ namespace Splitio.Services.SplitFetcher
         private static string SplitChangesUrlTemplate = ConfigurationManager.AppSettings["SPLIT_CHANGES_URL_TEMPLATE"];
         private static string UrlParameter_Since = ConfigurationManager.AppSettings["URL_PARAMETER_SINCE"];
         private static readonly ILog Log = LogManager.GetLogger(typeof(SplitSdkApiClient));
+        private const string SplitFetcherTime = "splitChangeFetcher.time";
+        private const string SplitFetcherStatus = "splitChangeFetcher.status.{0}";
+        private const string SplitFetcherException = "splitChangeFetcher.exception";
 
-        public SplitSdkApiClient(HTTPHeader header, string baseUrl, long connectionTimeOut, long readTimeout) : base(header, baseUrl, connectionTimeOut, readTimeout) { }
+
+        public SplitSdkApiClient(HTTPHeader header, string baseUrl, long connectionTimeOut, long readTimeout, IMetricsLog metricsLog = null) : base(header, baseUrl, connectionTimeOut, readTimeout, metricsLog) { }
 
         public string FetchSplitChanges(long since)
         {
+            var clock = new Stopwatch();
+            clock.Start();
             try
             {
                 var requestUri = GetRequestUri(since);
                 var response = ExecuteGet(requestUri);
                 if (response.statusCode == HttpStatusCode.OK)
                 {
+                    if (metricsLog != null)
+                    {
+                        metricsLog.Time(SplitFetcherTime, clock.ElapsedMilliseconds);
+                        metricsLog.Count(String.Format(SplitFetcherStatus, response.statusCode), 1);
+                    }
+
                     return response.content;
                 }
                 else
                 {
                     Log.Error(String.Format("Http status executing FetchSplitChanges: {0} - {1}", response.statusCode.ToString(), response.content));
+
+                    if (metricsLog != null)
+                    {
+                        metricsLog.Count(String.Format(SplitFetcherStatus, response.statusCode), 1);
+                    }
+
                     return String.Empty;
                 }
             }
             catch (Exception e)
             {
                 Log.Error("Exception caught executing FetchSplitChanges", e);
+
+                if (metricsLog != null)
+                {
+                    metricsLog.Count(SplitFetcherException, 1);
+                }
+
                 return String.Empty;
             }
         }
