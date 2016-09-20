060d25d75d155166eeb51f5efb82bfa4c8a07059
julianwaimann <jwaimann@makingsense.com>
2016-09-20 12:29:22 -0300

    Add clarification comment

--- NetSDK/Services/SegmentFetcher/Classes/SegmentTaskQueue.cs
+++ NetSDK/Services/SegmentFetcher/Classes/SegmentTaskQueue.cs
@@ -8,6 +8,7 @@ namespace Splitio.Services.SegmentFetcher.Classes
 {
     public static class SegmentTaskQueue
     {
+        //ConcurrentQueue<T> performs best when one dedicated thread is queuing and one dedicated thread is de-queuing
         public static ConcurrentQueue<SelfRefreshingSegment> segmentsQueue = new ConcurrentQueue<SelfRefreshingSegment>();
     }
 }
