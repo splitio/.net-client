14a49723a56773a7bb28c2da2cfd0d3c416aeae2
julianwaimann <jwaimann@makingsense.com>
2016-09-23 17:45:01 -0300

    Implement first benchmark version

---
+++ Microbenchmark/Benchmark.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading.Tasks;
+using Splitio.Services.Client.Classes;
+﻿using BenchmarkDotNet.Attributes;
+using Splitio.Services.Client.Interfaces;
+
+namespace Microbenchmark
+{
+    public class Benchmark
+    {
+        ISplitClient localhostClient;
+        ISplitClient jsonClient;
+
+        public Benchmark()
+        {
+            String apikey = "localhost";
+
+            var factory = new SplitFactory();
+            var configurations = new ConfigurationOptions();
+            configurations.LocalhostFilePath = @"C:\Making Sense\net-client\Microbenchmark\bin\Release\Resources\test.splits";
+            localhostClient = factory.BuildSplitClient(apikey, configurations);
+
+            jsonClient = new JSONFileClient(@"C:\Making Sense\net-client\Microbenchmark\bin\Release\Resources\splits_staging.json", @"C:\Making Sense\net-client\Microbenchmark\bin\Release\Resources\segment_payed.json");
+        }
+
+        [Benchmark]
+        public string GetTreatmentBenchmarkLocalhostA()
+        {
+            var result = localhostClient.GetTreatment("test", "double_writes_to_cassandra");
+            return result;
+        }
+
+        [Benchmark]
+        public string GetTreatmentBenchmarkLocalhostB()
+        {
+            var result = localhostClient.GetTreatment("test", "other_test_feature");
+            return result;
+        }
+
+        [Benchmark]
+        public string GetTreatmentBenchmarkJsonClientA()
+        {
+            var result = jsonClient.GetTreatment("test", "Marcio_Test_1");
+            return result;
+        }
+
+        [Benchmark]
+        public string GetTreatmentBenchmarkJsonClientB()
+        {
+            var result = jsonClient.GetTreatment("abcdfghijklm9878979", "Marcio_Test_1");
+            return result;
+        }
+    }
+}
---
+++ Microbenchmark/Program.cs
@@ -0,0 +1,17 @@
+﻿using BenchmarkDotNet.Running;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Microbenchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<Benchmark>();
+            Console.Read();
+        }
+    }
+}
