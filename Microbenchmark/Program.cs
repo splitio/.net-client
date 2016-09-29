0fb3ac72ea50ff554c317d84413cf366a4b56dad
julianwaimann <jwaimann@makingsense.com>
2016-09-29 11:53:16 -0300

    Implement metrics.net and profiler

--- Microbenchmark/Program.cs
+++ Microbenchmark/Program.cs
@@ -1,4 +1,5 @@
 ﻿using BenchmarkDotNet.Running;
+using Metrics;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,13 +11,19 @@ namespace Microbenchmark
     {
         static void Main(string[] args)
         {
-            var switcher = new BenchmarkSwitcher(new[] {
+            Metric.Config
+            .WithHttpEndpoint("http://localhost:2345/")
+            .WithAllCounters();
+
+            var metricsProcessor = new MetricsProcessor();
+            metricsProcessor.ProcessRequest(Int32.Parse(args[0]), Int32.Parse(args[1]));
+            /*var switcher = new BenchmarkSwitcher(new[] {
                 typeof(BenchmarkGetTreatmentLocalhost),
                 typeof(BenchmarkGetTreatmentStagingTestingAccount),
                 typeof(BenchmarkGetTreatmentStagingGoogleJSAccount)
             });
             switcher.Run(args);
-            Console.Read();
+            Console.Read();*/
         }
     }
 }
