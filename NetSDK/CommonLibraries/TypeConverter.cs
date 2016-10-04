ad4e594dbdaa88888744c2dc06a5752ccb70c659
julianwaimann <jwaimann@makingsense.com>
2016-10-04 12:26:56 -0300

    Add TypeConverter unit tests and removed unused method

--- NetSDK/CommonLibraries/TypeConverter.cs
+++ NetSDK/CommonLibraries/TypeConverter.cs
@@ -24,14 +24,5 @@ namespace Splitio.CommonLibraries
             }
             return null;
         }
-
-        public static DateTime Truncate(this DateTime dateTime, TimeSpan timeSpan)
-        {
-            if (timeSpan == TimeSpan.Zero)
-            {
-                return dateTime;
-            }
-            return dateTime.AddTicks(-(dateTime.Ticks % timeSpan.Ticks));
-        }
     }
 }
