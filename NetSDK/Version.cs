594615276a902b87bf440d91d99c22c959c24f53
julianwaimann <jwaimann@makingsense.com>
2017-01-24 16:48:45 -0300

    Implement update in version for release

--- NetSDK/Version.cs
+++ NetSDK/Version.cs
@@ -3,7 +3,7 @@ namespace Splitio
 {
     public static class Version
     {
-        public static string SplitSdkVersion = "2.0.5-rc100";
+        public static string SplitSdkVersion = "2.0.5";
         public static string SplitSpecVersion = "1.0";
     }
 }
