f3d5fcfa7fe11e566294d0579101158f0b0078fc
julianwaimann <jwaimann@makingsense.com>
2017-02-02 13:45:05 -0300

    Fix value format for impressions cache

--- NetSDK/Services/Cache/Classes/RedisImpressionsCache.cs
+++ NetSDK/Services/Cache/Classes/RedisImpressionsCache.cs
@@ -22,7 +22,8 @@ namespace Splitio.Services.Cache.Classes
         public void AddImpression(KeyImpression impression)
         {
             var key = impressionKeyPrefix + impression.feature;
-            var impressionJson = JsonConvert.SerializeObject(impression);
+            var impressionToSend = new{ keyName = impression.keyName, treatment = impression.treatment, time = impression.time };
+            var impressionJson = JsonConvert.SerializeObject(impressionToSend);
             redisAdapter.SAdd(key, impressionJson);
         }
 
