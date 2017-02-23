5f5a5d09cb545a75a516b604ed88c72d0f64faf3
julianwaimann <jwaimann@makingsense.com>
2017-02-23 17:17:31 -0300

    Updates in version for deploy

--- NetSDK/Version.cs
+++ NetSDK/Version.cs
@@ -3,7 +3,7 @@ namespace Splitio
 {
     public static class Version
     {
-        public static string SplitSdkVersion = "2.1.0-rc1";
+        public static string SplitSdkVersion = "2.1.0-rc2";
         public static string SplitSpecVersion = "1.0";
     }
 }
