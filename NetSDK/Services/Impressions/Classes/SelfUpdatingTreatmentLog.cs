0acdf2e94fe8f97452af7605c8667842904dd760
julianwaimann <jwaimann@makingsense.com>
2017-03-31 17:40:45 -0300

    Implement some refactorings ITreatmentLog to  IImpressionListener

--- NetSDK/Services/Impressions/Classes/SelfUpdatingTreatmentLog.cs
+++ NetSDK/Services/Impressions/Classes/SelfUpdatingTreatmentLog.cs
@@ -15,7 +15,7 @@ using System.Threading.Tasks;
 
 namespace Splitio.Services.Impressions.Classes
 {
-    public class SelfUpdatingTreatmentLog: ITreatmentLog
+    public class SelfUpdatingTreatmentLog: IImpressionListener
     {
         private ITreatmentSdkApiClient apiClient;
         private int interval;
@@ -75,9 +75,8 @@ namespace Splitio.Services.Impressions.Classes
         }
 
 
-        public void Log(string matchingKey, string feature, string treatment, long time, long? changeNumber, string label, string bucketingKey = null)
+        public void Log(KeyImpression impression)
         {
-            KeyImpression impression = new KeyImpression() { feature = feature, keyName = matchingKey, treatment = treatment, time = time, changeNumber = changeNumber, label = label, bucketingKey = bucketingKey };
             var enqueueTask = new Task(() => impressionsCache.AddImpression(impression));
             enqueueTask.Start();
         }
