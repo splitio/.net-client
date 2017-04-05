72fa644993c1bbfdcb51881609372c84a87470f5
julianwaimann <jwaimann@makingsense.com>
2017-04-05 16:38:30 -0300

    Update version to release it independently from Traffic Allocation

--- NetSDK/Version.cs
+++ NetSDK/Version.cs
@@ -3,7 +3,7 @@ namespace Splitio
 {
     public static class Version
     {
-        public static string SplitSdkVersion = "2.2.0-rc23";
+        public static string SplitSdkVersion = "2.2.1-rc1";
         public static string SplitSpecVersion = "1.0";
     }
 }
