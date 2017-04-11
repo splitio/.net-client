54759298ec1e6276df091c9688ab18e773d67c49
julianwaimann <jwaimann@makingsense.com>
2017-04-11 17:11:05 -0300

    Update version number for publish

--- NetSDK/Version.cs
+++ NetSDK/Version.cs
@@ -3,7 +3,7 @@ namespace Splitio
 {
     public static class Version
     {
-        public static string SplitSdkVersion = "3.0.0-rc63";
+        public static string SplitSdkVersion = "3.0.0-rc67";
         public static string SplitSpecVersion = "1.0";
     }
 }
