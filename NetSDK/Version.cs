19004fd399dd095e012f844ec163cb6eb49c4cfb
julianwaimann <jwaimann@makingsense.com>
2017-05-03 16:37:38 -0300

    Implement fix for TLS in version 2.1.4

--- NetSDK/Version.cs
+++ NetSDK/Version.cs
@@ -3,7 +3,7 @@ namespace Splitio
 {
     public static class Version
     {
-        public static string SplitSdkVersion = "2.1.4";
+        public static string SplitSdkVersion = "2.1.5";
         public static string SplitSpecVersion = "1.0";
     }
 }
