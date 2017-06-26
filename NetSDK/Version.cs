f8850adc4deb48e7979f52ca75ac2c3e549dc10b
julianwaimann <jwaimann@makingsense.com>
2017-06-26 17:56:12 -0300

    Update version for prerelease

--- NetSDK/Version.cs
+++ NetSDK/Version.cs
@@ -3,7 +3,7 @@ namespace Splitio
 {
     public static class Version
     {
-        public static string SplitSdkVersion = "3.2.0-rc272";
+        public static string SplitSdkVersion = "3.2.0-rc274";
         public static string SplitSpecVersion = "1.0";
     }
 }
