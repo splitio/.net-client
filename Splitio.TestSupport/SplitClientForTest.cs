6eb5545c90a8533b4585ab75772a136f459ff72f
julianwaimann <jwaimann@makingsense.com>
2017-07-06 12:22:25 -0300

    Implement test support for net sdk

---
+++ Splitio.TestSupport/SplitClientForTest.cs
@@ -0,0 +1,53 @@
+﻿using Splitio.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Splitio.Services.Client.Classes
+{
+    public class SplitClientForTest : SplitClient
+    {
+        private Dictionary<string, string> _tests;
+
+        public SplitClientForTest()
+        {
+            _tests = new Dictionary<string, string>();
+        }
+
+        public override void Destroy()
+        {
+            _tests = new Dictionary<string, string>();
+        }
+
+        public void ClearTreatments() 
+        {
+            _tests = new Dictionary<string, string>();
+        }
+
+        public void RegisterTreatments(Dictionary<string, string> treatments)
+        {
+            _tests.Union(treatments);
+        }
+
+        public void RegisterTreatment(string feature, string treatment)
+        {
+            _tests.Add(feature, treatment);
+        }
+
+        public string GetTreatment(string key, string feature)
+        {
+            return _tests.ContainsKey(feature) ? _tests[feature]: "control";
+        }
+
+        public string GetTreatment(string key, string feature, Dictionary<string, object> attributes)
+        {
+            return this.GetTreatment(key, feature);
+        }
+
+        public string GetTreatment(Key key, string feature, Dictionary<string, object> attributes)
+        {
+            return _tests.ContainsKey(feature) ? _tests[feature] : "control";
+        }
+    }
+}
---
+++ Splitio.TestSupport/SplitSuite.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Xunit.Extensions;
+
+namespace Splitio.TestSupport
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = true, Inherited = true)]
+    public class SplitSuite : ClassDataAttribute
+    {
+        public SplitScenario[] splitScenarios { get; private set; }
+
+        public SplitSuite(string scenarios): base(typeof(object))
+        {
+            this.splitScenarios = JsonConvert.DeserializeObject<SplitScenario[]>(scenarios);
+        }
+
+        public override IEnumerable<object[]> GetData(MethodInfo methodUnderTest, Type[] parameterTypes)
+        {
+            var data = new List<string[]>();
+            foreach (var splitScenario in this.splitScenarios)
+            {
+                foreach (var splitTest in splitScenario.features)
+                {
+                    foreach (var treatment in splitTest.treatments)
+                    {
+                        data.Add(new string[] { splitTest.feature, treatment });
+                    }
+                }
+            }
+            return data;
+        }
+    }
+}
