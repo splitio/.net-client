3a6a61da32ac608883b6856a0a8161e93ed436ca
julianwaimann <jwaimann@makingsense.com>
2017-08-01 17:17:36 -0300

    Update version for release

--- NetSDK/Version.cs
+++ NetSDK/Version.cs
@@ -3,7 +3,7 @@ namespace Splitio
 {
     public static class Version
     {
-        public static string SplitSdkVersion = "3.2.1-rc1";
+        public static string SplitSdkVersion = "3.2.1";
         public static string SplitSpecVersion = "1.0";
     }
 }
