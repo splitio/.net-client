2677cc8cdb4de96c4c39e2b71d5c20d05cef1f2e
mssplitio <mssplitio@gmail.com>
2019-04-23 01:28:55 -0300

    Return config as string

--- Net SDK Unit Tests/Unit Tests/Client/SplitManagerUnitTests.cs
+++ Net SDK Unit Tests/Unit Tests/Client/SplitManagerUnitTests.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
 using Splitio.Domain;
 using Splitio.Services.Cache.Classes;
 using Splitio.Services.Client.Classes;
@@ -372,7 +373,7 @@ namespace Splitio_Tests.Unit_Tests.Client
         public void Splits_WithConfigs_ReturnSuccessfully()
         {
             //Arrange
-            var configurations = new { On = new { Name = "Test Config" } };
+            var configurations = JsonConvert.SerializeObject(new { On = new { Name = "Test Config" } });
             var conditionsWithLogic = new List<ConditionWithLogic>();
             var conditionWithLogic = new ConditionWithLogic()
             {
@@ -411,7 +412,7 @@ namespace Splitio_Tests.Unit_Tests.Client
         public void Split_WithConfigs_ReturnSuccessfully()
         {
             //Arrange
-            var configurations = new { On = new { Name = "Test Config" } };
+            var configurations = JsonConvert.SerializeObject(new { On = new { Name = "Test Config" } });
             var conditionsWithLogic = new List<ConditionWithLogic>();
             var conditionWithLogic = new ConditionWithLogic()
             {
